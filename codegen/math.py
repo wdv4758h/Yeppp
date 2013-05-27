ecfe713e6bf61a48cd8e52601c7515509b6f9095
Marat Dukhan <mdukhan3@gatech.edu>
2013-05-27 17:37:06 -0400

    Added polynomial evaluation functions Refactored trigonometric functions Added vector cos function Added Haswell version of vector sin Improved performance of vector sin on Bulldozer/Pilerdriver

--- codegen/math.py
+++ codegen/math.py
@@ -96,6 +96,7 @@ def generate_sin(module):
 		function.assembly_implementations.append(yeppp.library.math.x64.Sin_V64f_V64f_Nehalem)
 		function.assembly_implementations.append(yeppp.library.math.x64.Sin_V64f_V64f_SandyBridge)
 		function.assembly_implementations.append(yeppp.library.math.x64.Sin_V64f_V64f_Bulldozer)
+		function.assembly_implementations.append(yeppp.library.math.x64.Sin_V64f_V64f_Haswell)
 
 		function.c_documentation = """
 @brief	Computes sine on %(InputType0)s elements.
@@ -129,9 +130,48 @@ return YepStatusOk;
 """
 		function.generate("yepMath_Sin_V64f_V64f(x, y, YepSize length)")
 
+def generate_cos(module):
+	with yeppp.module.Function(module, 'Cos', 'Cosine') as function:
+		function.assembly_implementations.append(yeppp.library.math.x64.Cos_V64f_V64f_Nehalem)
+		function.assembly_implementations.append(yeppp.library.math.x64.Cos_V64f_V64f_SandyBridge)
+		function.assembly_implementations.append(yeppp.library.math.x64.Cos_V64f_V64f_Bulldozer)
+		function.assembly_implementations.append(yeppp.library.math.x64.Cos_V64f_V64f_Haswell)
+
+		function.c_documentation = """
+@brief	Computes cosine on %(InputType0)s elements.
+@param[in]	x	Pointer the input array.
+@param[out]	y	Pointer the output array.
+@param[in]	length	The length of the arrays pointed by @a x and @a y.
+@retval	#YepStatusOk	The computations finished successfully.
+@retval	#YepStatusNullPointer	One of the @a x or @a y arguments is null.
+@retval	#YepStatusMisalignedPointer	One of the @a x or @a y arguments is not properly aligned.
+"""
+		function.java_documentation = """
+@brief	Computes cosine on %(InputType0)s elements.
+@param[in]	xArray	Input array.
+@param[in]   xOffset Offset of the first element in @a xArray.
+@param[out]	yArray	Output array.
+@param[in]   yOffset Offset of the first element in @a yArray.
+@param[in]	length	The length of the subarrays to be used in computation.
+@throws	NullPointerException	If @a xArray or @a yArray argument is null.
+@throws	InvalidArgumentException	If the @a xOffset or @a yOffset argument is negative.
+@throws	NegativeArraySizeException	If the @a length argument is null.
+@throws  IndexOutOfBoundsException If @a xOffset + @a length exceeds the length of @a xArray array or @a yOffset + @a length exceeds the length of @a yArray array.
+@throws	MisalignedPointerError	If one of the arrays is not properly aligned.
+"""
+		function.c_implementation = """
+while (length-- != 0) {
+	const Yep%(InputType0)s x = *xPointer++;
+	const Yep%(OutputType0)s y = yepBuiltin_Cos_%(InputType0)s_%(OutputType0)s(x);
+	*yPointer++ = y;
+}
+return YepStatusOk;
+"""
+		function.generate("yepMath_Cos_V64f_V64f(x, y, YepSize length)")
+
 def generate_tan(module):
 	with yeppp.module.Function(module, 'Tan', 'Tangent') as function:
-		function.assembly_implementations.append(yeppp.library.math.x64.Tan_V64f_V64f_Bulldozer)
+# 		function.assembly_implementations.append(yeppp.library.math.x64.Tan_V64f_V64f_Bulldozer)
 	
 		function.c_documentation = """
 @brief	Computes tangent on %(InputType0)s elements.
@@ -165,10 +205,51 @@ return YepStatusOk;
 """
 		function.generate("yepMath_Tan_V64f_V64f(x, y, YepSize length)")
 
+def generate_evaluate_polynomial(module):
+	with yeppp.module.Function(module, 'EvaluatePolynomial', 'Polynomial evaluation') as function:
+		function.assembly_implementations.append(yeppp.library.math.x64.EvaluatePolynomial_VfVf_Vf_SSE2)
+		function.assembly_implementations.append(yeppp.library.math.x64.EvaluatePolynomial_V64fV64f_V64f_Bonnell)
+		function.assembly_implementations.append(yeppp.library.math.x64.EvaluatePolynomial_V32fV32f_V32f_Bonnell)
+		function.assembly_implementations.append(yeppp.library.math.x64.EvaluatePolynomial_VfVf_Vf_Nehalem)
+		function.assembly_implementations.append(yeppp.library.math.x64.EvaluatePolynomial_VfVf_Vf_SandyBridge)
+		function.assembly_implementations.append(yeppp.library.math.x64.EvaluatePolynomial_VfVf_Vf_Bulldozer)
+		function.assembly_implementations.append(yeppp.library.math.x64.EvaluatePolynomial_VfVf_Vf_Haswell)
+
+		function.c_documentation = """
+@brief	Evaluates polynomial with %(InputType0)s coefficients on an array of %(InputType0)s elements.
+@param[in]	x	Pointer to the array of elements on which the polynomial will be evaluated.
+@param[in]	coef	Pointer to the array of polynomial coefficients.
+@param[out]	y	Pointer the array where the result of polynomial evaluation will be stored.
+@param[in]	coefCount	Number of polynomial coefficients. This should equal the polynomial degree plus one.
+@param[in]	length	Number of elements in the arrays specified by @a x and @a y.
+@retval	#YepStatusOk	The computations finished successfully.
+@retval	#YepStatusNullPointer	Either @a x or @a y arguments is null.
+@retval	#YepStatusMisalignedPointer	Either @a x or @a y arguments is not properly aligned.
+"""
+		function.c_implementation = """
+if YEP_UNLIKELY(coefCount == 0) {
+	return YepStatusInvalidArgument;
+}
+while (length-- != 0) {
+	const Yep%(InputType0)s x = *xPointer++;
+	Yep%(OutputType0)s y = coefPointer[coefCount - 1];
+	for (YepSize coefIndex = coefCount - 1; coefIndex != 0; coefIndex--) {
+		const Yep%(InputType0)s coef = coefPointer[coefIndex - 1];
+		y = yepBuiltin_MultiplyAdd_%(OutputType0)s%(InputType1)s%(InputType0)s_%(OutputType0)s(y, x, coef);
+	}
+	*yPointer++ = y;
+}
+return YepStatusOk;
+"""
+		function.generate("yepMath_EvaluatePolynomial_V32fV32f_V32f(coef[coefCount], x, y, YepSize coefCount, YepSize length)")
+		function.generate("yepMath_EvaluatePolynomial_V64fV64f_V64f(coef[coefCount], x, y, YepSize coefCount, YepSize length)")
+
 if __name__ == '__main__':
 	with yeppp.module.Module('Math', 'Vector mathematical functions') as module:
 		generate_log(module)
 		generate_exp(module)
 		generate_sin(module)
+		generate_cos(module)
 		generate_tan(module)
+		generate_evaluate_polynomial(module)
 # 		generate_sqrt(module)
