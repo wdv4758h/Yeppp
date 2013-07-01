09555a0b4b0727e8cfe97c504e5f306e051dafe5
Marat Dukhan <mdukhan3@gatech.edu>
2013-07-01 13:29:10 -0400

    Additional single-precision runtime functions: __aeabi_fcmpge/le/eq/ne

--- runtime/sources/arm/aeabi_fcmple.asm
+++ runtime/sources/arm/aeabi_fcmple.asm
@@ -15,23 +15,23 @@ BEGIN_ARM_FUNCTION __aeabi_fcmple
 
 	/* Check if a is NaN */
 	CMP r12, r0, LSL #1
-	/* If a is NaN, make it positive to guarantee that it is greater than any b */
+	/* If b is NaN, make it positive to guarantee that its value (0x7F8nnnnn) is greater than any b */
 	ANDLO r0, r0, #0x7FFFFFFF
 	
 	/* Check if b is NaN */
 	CMP r12, r1, LSL #1
-	/* If b is NaN, make it negative to guarantee that it is less than any a */
+	/* If a is NaN, make it negative to guarantee that its value (0xFF8nnnnn) is less than any a */
 	ORRLO r1, r1, #0x80000000
 	
 	/* Check if a is negative */
 	CMP r0, #0
 	/* If negative than a = 0x80000000 - a */
-	RSBMI r0, r0, r12, LSL #7
+	RSBMI r0, r0, #0x80000000
 	
 	/* Check if b is negative */
 	CMP r1, #0
 	/* If negative than b = 0x80000000 - b */
-	RSBMI r1, r1, r12, LSL #7
+	RSBMI r1, r1, #0x80000000
 
 	/* Normal case */
 	CMP r0, r1
