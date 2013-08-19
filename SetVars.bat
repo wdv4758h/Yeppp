a64b4ba8e631529d6982cc0681d75ce4c5b884d4
Marat Dukhan <mdukhan3@gatech.edu>
2013-08-19 16:18:43 -0400

    C/C++ library: Android integration Setup scripts: Set YEPBINARIES variable and Visual Studio variables on Windows

--- SetVars.bat
+++ SetVars.bat
@@ -19,19 +19,45 @@ IF /i %PROCESSOR_ARCHITECTURE% == x86 GOTO x86
 ECHO Error: unknown system architecture %processor_architecture%
 GOTO help
 
-:x86
-SET YEPROOT=%~dp0
-SET YEPPLATFORM=x86-windows-default-i586
-SET PATH=%YEPROOT%binaries\windows\x86;%PATH%
-IF EXIST %YEPROOT%binaries\java-1.5\yeppp.jar SET CLASSPATH=%YEPROOT%binaries\java-1.5\yeppp.jar;%CLASSPATH%
+:universal
+SET "PATH=%YEPBINARIES%;%PATH%"
+IF EXIST %YEPROOT%binaries\java-1.5\yeppp.jar (
+	IF "%CLASSPATH%" == "" (
+		SET "CLASSPATH=%YEPROOT%binaries\java-1.5\yeppp.jar"
+	) ELSE (
+		SET "CLASSPATH=%YEPROOT%binaries\java-1.5\yeppp.jar;%CLASSPATH%"
+	)
+)
+IF "%INCLUDE%" == "" (
+	SET "INCLUDE=%YEPROOT%library\headers"
+) ELSE (
+	SET "INCLUDE=%YEPROOT%library\headers;%INCLUDE%"
+)
+IF "%LIB%" == "" (
+	SET "LIB=%YEPROOT%library\headers"
+) ELSE (
+	SET "LIB=%YEPROOT%library\headers;%LIB%"
+)
+IF EXIST "%YEPROOT%binaries\android\yeppp\Android.mk" (
+	IF "%NDK_MODULE_PATH%" == "" (
+		SET "NDK_MODULE_PATH=%YEPROOT:\=/%binaries/android"
+	) ELSE (
+		SET "NDK_MODULE_PATH=%YEPROOT:\=/%binaries/android:%NDK_MODULE_PATH%"
+	)
+)
 EXIT /B 0
 
+:x86
+SET "YEPROOT=%~dp0"
+SET "YEPBINARIES=%YEPROOT%binaries\windows\x86"
+SET YEPPLATFORM=x86-windows-default-i586
+GOTO universal
+
 :x64
-SET YEPROOT=%~dp0
+SET "YEPROOT=%~dp0"
+SET "YEPBINARIES=%YEPROOT%binaries\windows\amd64"
 SET YEPPLATFORM=x64-windows-ms-default
-SET PATH=%YEPROOT%binaries\windows\amd64;%PATH%
-IF EXIST %YEPROOT%binaries\java-1.5\yeppp.jar SET CLASSPATH=%YEPROOT%binaries\java-1.5\yeppp.jar;%CLASSPATH%
-EXIT /B 0
+GOTO universal
 
 :error
 ECHO Error: invalid command-line argument(s)
@@ -43,6 +69,9 @@ ECHO Possible options for [param]
 ECHO     x86 - set variables for 32-bit (x86) environment
 ECHO     x64 - set variables for 64-bit (x86-64 aka x64) environment
 ECHO.    /?  - show this help message
-ECHO If neither option is specified, the variable are set based on OS architecture.
-IF "%1" == "/?" EXIT /B 0
-ELSE EXIT /B 1
+ECHO If neither option is specified, the variables are set based on OS architecture.
+IF "%1" == "/?" (
+	EXIT /B 0
+) ELSE (
+	EXIT /B 1
+)
