d7e8207dd30d430cf6a7e14f7739fba7e2a920a3
Marat Dukhan <mdukhan3@gatech.edu>
2014-01-02 10:33:05 -0500

    Build system: clean target for Runtime library

--- runtime/make.cmd
+++ runtime/make.cmd
@@ -4,4 +4,10 @@ REM
 REM This file is part of Yeppp! library and licensed under the New BSD license.
 REM See LICENSE.txt for the full text of the license.
 
-call "%~dp0..\make.cmd" %1
+IF "%1" == "clean" GOTO CLEAN
+CALL "%~dp0..\make.cmd" %1
+GOTO :eof
+
+:CLEAN
+RD /S /Q binaries 2>NUL
+GOTO :eof
