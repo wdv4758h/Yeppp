71395544561786884a35c1512cce05906a2067e6
Marat Dukhan <mdukhan3@gatech.edu>
2014-01-03 06:25:08 -0500

    Build system: removed GAS configuration from Android/x86

--- cmake/android-x86.toolchain.cmake
+++ cmake/android-x86.toolchain.cmake
@@ -10,7 +10,6 @@ endif()
 include(CMakeForceCompiler)
 CMAKE_FORCE_C_COMPILER(i686-linux-android-gcc GNU)
 CMAKE_FORCE_CXX_COMPILER(i686-linux-android-g++ GNU)
-set(CMAKE_ASM-ATT_COMPILER i686-linux-android-as)
 
 set(ANDROID_SYSROOT "$ENV{ANDROID_NDK_ROOT}/platforms/android-9/arch-x86")
 # Headers
@@ -23,11 +22,6 @@ set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -nostdlib -fPIC -Wno-psabi -fno-unwind-table
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -ffunction-sections -fdata-sections -O3 -fomit-frame-pointer" CACHE STRING "")
 # C++-specific options
 set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -fno-rtti -fno-exceptions" CACHE STRING "")
-
-# Target-specific options
-set(CMAKE_ASM-ATT_FLAGS "-march=mips32")
-# Code-generation options
-set(CMAKE_ASM-ATT_FLAGS "${CMAKE_ASM_FLAGS} --noexecstack" CACHE STRING "")
 
 # Linker options
 set(CMAKE_SHARED_LINKER_FLAGS "-fPIC -nostdlib --sysroot=${ANDROID_SYSROOT} -Wl,-z,noexecstack -Wl,--no-undefined -Wl,--gc-sections" CACHE STRING "")
