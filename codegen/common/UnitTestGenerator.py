18c2f8d2e7bd1bdc19a624615532d86abe4375b9
Robert Guthrie <rguthrie3@gatech.edu>
2016-04-06 17:50:21 -0400

    Generating unit tests, still need to change the range for sampling from uniform

--- codegen/common/UnitTestGenerator.py
+++ codegen/common/UnitTestGenerator.py
@@ -20,11 +20,8 @@ class UnitTestGenerator:
     def __init__(self, name, arguments):
         self.name = name
         self.arguments = arguments
-
-        # Additional locals:
-        # TODO need to set these at some point
-        self.ref_array = None
-        self.init_array = None
+        self.inputs_arrs = [ arg for arg in self.arguments if arg.is_pointer and arg.is_const ]
+        self.output = [ arg for arg in self.arguments if not arg.arg_type == "YepSize" and not arg.is_const ][0]
 
     def generate_unit_test(self):
         """
@@ -44,6 +41,7 @@ class UnitTestGenerator:
         self._argument_loop_body(utg)
         utg.add_line("for (YepSize length = 1024; length < 1088; length++) {").indent()
         self._argument_loop_body(utg)
+        utg.dedent()
 
         indent_depth = len([ arg for arg in self.arguments if arg.is_pointer ])
         for i in range(indent_depth):
@@ -56,32 +54,6 @@ class UnitTestGenerator:
         utg.add_line("return -failedTests;")
         utg.dedent().add_line("}")
         return utg.get_code()
-
-    def _passed_or_skipped(self, utg):
-        utg.add_line("reportPassedTest(\"{}\", descriptor->microarchitecture);".format(self.name))
-        utg.dedent().add_line("} else {").indent()
-        utg.add_line("reportSkippedTest(\"{}\", descriptor->microarchitecture);".format(self.name))
-        utg.dedent().add_line("}")
-
-    def _argument_loop_body(self, utg):
-        utg.add_line("memcpy({0}, {1}, sizeof({0}))".format(self.ref_array, self.init_array))
-        inputs = [ arg for arg in self.arguments if arg.is_const ]
-        output = [ arg for arg in self.arguments if not arg.is_const and not arg.arg_type == "YepSize" ][0]
-        utg.add_line("status = defaultImplementation({}, {}, length);".format(
-            ", ".join([ "&" + i.name + "Array[" + i.name + "Offset]" for i in inputs ]),
-            "&" + output.name + "RefArray[" + output.name + "Offset]"))
-        utg.add_line("assert(status == YepStatusOk);")
-        utg.add_line()
-
-        utg.add_line("memcpy({0}Array, {0}InitArray, sizeof({0}Array));".format(output.name))
-        utg.add_line("status = descriptor->function({}, {}, length);".format(
-            ", ".join([ "&" + i.name + "Array[" + i.name + "Offset]" for i in inputs ]),
-            "&" + output.name + "Array[" + i.name + "Offset]"))
-        utg.add_line("if (memcmp({0}Array, {0}RefArray, sizeof({0}Array)) != 0) {{".format(output.name)).indent()
-        utg.add_line("failedTests++;")
-        utg.add_line("reportFailedTest(\"{}\", descriptor->microarchitecture);".format(self.name))
-        utg.add_line("goto next_descriptor;")
-        utg.dedent().add_line("}").dedent().add_line("}").dedent()
 
     def _unit_test_signature(self, utg):
         utg.add_line("static Yep32s Test_{0}(Yep64u supportedIsaFeatures, Yep64u supportedSimdFeatures, Yep64u supportedSystemFeatures) {{".format(self._name_without_module())).indent()
@@ -104,15 +76,23 @@ class UnitTestGenerator:
             if arg.is_pointer:
                 utg.add_line("YEP_ALIGN(64) {0} {1}Array[1088 + (64 / sizeof({0}))];".format(
                     arg.arg_type, arg.name))
+                if arg.name == self.output.name: continue # We don't want to initialize the output array here, only the init array below
                 # TODO FIX THIS LINE!!!
                 utg.add_line("status = {0}(&rng, {1}, {2}, YEP_COUNT_OF({2}));".format(
                     random_generator_function_map[arg.arg_type], ", ".join(["-128", "127"]), arg.name))
+                utg.add_line("assert(status == YepStatusOk);")
             elif arg.arg_type != "YepSize":
                 utg.add_line("{} {};".format(arg.arg_type, arg.name))
                 # TODO FIX THIS LINE!!!
                 utg.add_line("status = {0}(&rng, {1}, &{2}, 1);".format(
                     random_generator_function_map[arg.arg_type], ", ".join(["-128", "127"]), arg.name))
-            utg.add_line("assert(status == YepStatusOk);")
+                utg.add_line("assert(status == YepStatusOk);")
+        # Initial arrays and reference arrays:
+        if self.output.is_pointer: # Output is an array (not max, min, etc.)
+            utg.add_line("YEP_ALIGN(64) {0} {1}InitArray[1088 + (64 / sizeof({0}))];".format(self.output.arg_type, self.output.name))
+            utg.add_line("YEP_ALIGN(64) {0} {1}RefArray[1088 + (64 / sizeof({0}))];".format(self.output.arg_type, self.output.name))
+            utg.add_line("status = {0}(&rng, {1}, {2}InitArray, YEP_COUNT_OF({2}));".format(
+                random_generator_function_map[self.output.arg_type], ", ".join(["-128", "127"]), self.output.name))
 
     def _begin_implementation_loop(self, utg):
         utg.add_line("for (DescriptorPointer descriptor = &_dispatchTable_{0}[0]; \
@@ -129,6 +109,43 @@ descriptor != defaultDescriptor; descriptor++) {{".format(self.name)).indent()
                     arg.name, arg.arg_type)).indent()
             elif arg.arg_type == "YepSize": # is the length argument
                 utg.add_line("for (YepSize length = 0; length < 64; length++) {").indent()
+
+    def _argument_loop_body(self, utg):
+        inputs = [ arg for arg in self.arguments if arg.is_const ]
+        output = [ arg for arg in self.arguments if not arg.is_const and not arg.arg_type == "YepSize" ][0]
+
+        utg.add_line("memcpy({0}RefArray, {0}InitArray, sizeof({0}RefArray));".format(self.output.name))
+
+        utg.add_line("status = defaultImplementation({});".format(", ".join(self._determine_func_call_args(True))))
+        utg.add_line("assert(status == YepStatusOk);")
+        utg.add_line()
+
+        utg.add_line("memcpy({0}Array, {0}InitArray, sizeof({0}Array));".format(self.output.name))
+        utg.add_line("status = descriptor->function({});".format(", ".join(self._determine_func_call_args(False))))
+        utg.add_line("if (memcmp({0}Array, {0}RefArray, sizeof({0}Array)) != 0) {{".format(self.output.name)).indent()
+        utg.add_line("failedTests++;")
+        utg.add_line("reportFailedTest(\"{}\", descriptor->microarchitecture);".format(self.name))
+        utg.add_line("goto next_descriptor;")
+        utg.dedent().add_line("}").dedent().add_line("}")
+
+    def _determine_func_call_args(self, is_default_impl_call):
+        call_args = []
+        for arg in self.arguments:
+            if arg.is_pointer:
+                if arg.is_const:
+                    call_args.append("&{0}Array[{0}Offset]".format(arg.name))
+                else:
+                    extra_part = "Ref" if is_default_impl_call else ""
+                    call_args.append("&{0}{1}Array[{0}Offset]".format(arg.name, extra_part))
+            else:
+                call_args.append(arg.name)
+        return call_args
+
+    def _passed_or_skipped(self, utg):
+        utg.add_line("reportPassedTest(\"{}\", descriptor->microarchitecture);".format(self.name))
+        utg.dedent().add_line("} else {").indent()
+        utg.add_line("reportSkippedTest(\"{}\", descriptor->microarchitecture);".format(self.name))
+        utg.dedent().add_line("}")
 
     def _name_without_module(self):
         """
--- codegen/generate-unit-test.py
+++ codegen/generate-unit-test.py
@@ -6,15 +6,162 @@ import os
 from common.Function import Function
 
 def generate_unit_tests(path, function_list):
-    for function in function_list:
-        print function.unit_test
-        exit()
+    with open(path, "w") as output:
+        output.write(
+"""
+#include <yepPredefines.h>
+#include <yepPrivate.h>
+#include <yepLibrary.h>
+#include <library/functions.h>
+#include <yepRandom.h>
+#include <core/functions.h>
+#include <yepBuiltin.h>
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+
+
+#ifdef YEP_WINDOWS_OS
+	#include <windows.h>
+	#define YEP_ESCAPE_NORMAL_COLOR ""
+	#define YEP_ESCAPE_RED_COLOR ""
+	#define YEP_ESCAPE_GREEN_COLOR ""
+	#define YEP_ESCAPE_YELLOW_COLOR ""
+#else
+	#define YEP_ESCAPE_NORMAL_COLOR "\x1B[0m"
+	#define YEP_ESCAPE_RED_COLOR "\x1B[31m"
+	#define YEP_ESCAPE_GREEN_COLOR "\x1B[32m"
+	#define YEP_ESCAPE_YELLOW_COLOR "\x1B[33m"
+#endif
+
+
+static const char* getMicroarchitectureName(YepCpuMicroarchitecture microarchitecture) {
+	const YepSize bufferSize = 1024;
+	static char buffer[bufferSize];
+	YepSize bufferLength = bufferSize - 1;
+	YepStatus status = yepLibrary_GetString(YepEnumerationCpuMicroarchitecture, microarchitecture, YepStringTypeDescription, buffer, &bufferLength);
+	assert(status == YepStatusOk);
+	buffer[bufferLength] = '\0';
+	return buffer;
+}
+
+static void reportFailedTest(const char* functionName, YepCpuMicroarchitecture microarchitecture) {
+	#ifdef YEP_WINDOWS_OS
+		CONSOLE_SCREEN_BUFFER_INFO bufferInfo;
+		::GetConsoleScreenBufferInfo(::GetStdHandle(STD_OUTPUT_HANDLE), &bufferInfo);
+		printf("%s (%s): ", functionName, getMicroarchitectureName(microarchitecture));
+		fflush(stdout);
+		::SetConsoleTextAttribute(::GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_INTENSITY);
+		printf("FAILED\n");
+		fflush(stdout);
+		::SetConsoleTextAttribute(::GetStdHandle(STD_OUTPUT_HANDLE), bufferInfo.wAttributes);
+	#else
+		printf("%s (%s): %sFAILED%s\n", functionName, getMicroarchitectureName(microarchitecture), YEP_ESCAPE_RED_COLOR, YEP_ESCAPE_NORMAL_COLOR);
+	#endif
+}
+
+static void reportFailedTest(const char* functionName, YepCpuMicroarchitecture microarchitecture, float ulpError) {
+	#ifdef YEP_WINDOWS_OS
+		CONSOLE_SCREEN_BUFFER_INFO bufferInfo;
+		::GetConsoleScreenBufferInfo(::GetStdHandle(STD_OUTPUT_HANDLE), &bufferInfo);
+		printf("%s (%s): ", functionName, getMicroarchitectureName(microarchitecture));
+		fflush(stdout);
+		::SetConsoleTextAttribute(::GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_INTENSITY);
+		printf("FAILED");
+		fflush(stdout);
+		::SetConsoleTextAttribute(::GetStdHandle(STD_OUTPUT_HANDLE), bufferInfo.wAttributes);
+		printf(" (%f ULP)\n", ulpError);
+	#else
+		printf("%s (%s): %sFAILED%s (%f ULP)\n", functionName, getMicroarchitectureName(microarchitecture), YEP_ESCAPE_RED_COLOR, YEP_ESCAPE_NORMAL_COLOR, ulpError);
+	#endif
+}
+
+static void reportPassedTest(const char* functionName, YepCpuMicroarchitecture microarchitecture) {
+	#ifdef YEP_WINDOWS_OS
+		CONSOLE_SCREEN_BUFFER_INFO bufferInfo;
+		::GetConsoleScreenBufferInfo(::GetStdHandle(STD_OUTPUT_HANDLE), &bufferInfo);
+		printf("%s (%s): ", functionName, getMicroarchitectureName(microarchitecture));
+		fflush(stdout);
+		::SetConsoleTextAttribute(::GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+		printf("PASSED\n");
+		fflush(stdout);
+		::SetConsoleTextAttribute(::GetStdHandle(STD_OUTPUT_HANDLE), bufferInfo.wAttributes);
+	#else
+		printf("%s (%s): %sPASSED%s\n", functionName, getMicroarchitectureName(microarchitecture), YEP_ESCAPE_GREEN_COLOR, YEP_ESCAPE_NORMAL_COLOR);
+	#endif
+}
+
+static void reportSkippedTest(const char* functionName, YepCpuMicroarchitecture microarchitecture) {
+	#ifdef YEP_WINDOWS_OS
+		CONSOLE_SCREEN_BUFFER_INFO bufferInfo;
+		::GetConsoleScreenBufferInfo(::GetStdHandle(STD_OUTPUT_HANDLE), &bufferInfo);
+		printf("%s (%s): ", functionName, getMicroarchitectureName(microarchitecture));
+		fflush(stdout);
+		::SetConsoleTextAttribute(::GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+		printf("SKIPPED\n");
+		fflush(stdout);
+		::SetConsoleTextAttribute(::GetStdHandle(STD_OUTPUT_HANDLE), bufferInfo.wAttributes);
+	#else
+		printf("%s (%s): %sSKIPPED%s\n", functionName, getMicroarchitectureName(microarchitecture), YEP_ESCAPE_YELLOW_COLOR, YEP_ESCAPE_NORMAL_COLOR);
+	#endif
+}
+
+
+""")
+        for function in function_list:
+            output.write(function.unit_test)
+            output.write("\n\n")
+
+        output.write("int main (int argc, char **argv) {\n")
+        boolean_names = [ "test" + "_".join(func.name.split("_")[1:]) for func in function_list ]
+        for name in boolean_names:
+            output.write("    YepBoolean {} = YepBooleanFalse;\n".format(name))
+        output.write("    if (argc == 1) {\n")
+        for name in boolean_names:
+            output.write("        {} = YepBooleanTrue;\n".format(name))
+        output.write(
+"""     else {
+        for (int i = 1; i < argc; i++) {
+""")
+        else_part = ""
+        for name in boolean_names:
+            output.write(
+"""        {}if (strcmp(argv[i], \"{}\") == 0) {{
+               {} = YepBooleanTrue;
+           }}
+""".format(else_part, "_".join(name.split("_")[1:]), name))
+            else_part = "else "
+        output.write(
+"""
+
+	YepStatus status = _yepLibrary_InitCpuInfo();
+	assert(status == YepStatusOk);
+
+	Yep64u supportedIsaFeatures, supportedSimdFeatures, supportedSystemFeatures;
+	status = yepLibrary_GetCpuIsaFeatures(&supportedIsaFeatures);
+	assert(status == YepStatusOk);
+	status = yepLibrary_GetCpuSimdFeatures(&supportedSimdFeatures);
+	assert(status == YepStatusOk);
+	status = yepLibrary_GetCpuSystemFeatures(&supportedSystemFeatures);
+	assert(status == YepStatusOk);
+
+        Yep32s failedTests = 0;
+""")
+        for name in boolean_names:
+            output.write(
+"""
+        if YEP_LIKELY({})
+            failedTests += {}(supportedIsaFeatures, supportedSimdFeatures, supportedSystemFeatures);
+""".format(name, "Test_{}".format(name[4:])))
+
+        output.write("    return failedTests;\n}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Unit Test Generator")
     parser.add_argument("-o", dest="output", required=True, help="Output file name")
     parser.add_argument("input", nargs=1)
+    parser.add_argument("-op", dest="op", required=True, help="Operation to generate tests for")
     options = parser.parse_args()
 
     with open(options.input[0], "r") as specification_file:
@@ -25,10 +172,11 @@ if __name__ == "__main__":
         func_list = []
         for op_set in yaml_data["functions"]:
             op = op_set["operation"]
+            if op != options.op: continue # Skip the operations that we don't care about
 
             for func_group in op_set["function_groups"]:
                 for func in func_group["group"]:
                     func_list.append(Function(func, func_group))
-    
+
     generate_unit_tests(options.output, func_list)
 
