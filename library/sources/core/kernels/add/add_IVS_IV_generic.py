3dad08f52a7b1190219886d5cbb602108bdcb226
Robert Guthrie <rguthrie3@gatech.edu>
2016-07-03 09:59:33 -0500

    scalar to immediate addition on nehalem and haswell

--- library/sources/core/kernels/add/add_IVS_IV_generic.py
+++ library/sources/core/kernels/add/add_IVS_IV_generic.py
@@ -4,10 +4,9 @@ from common.YepStatus import *
 from common.pipeline import software_pipelined_loop
 from add_common import *
 
-def add_IVV_IV_generic(arg_x, arg_y, arg_n, isa_ext):
+def add_IVS_IV_generic(arg_x, arg_y, arg_n, isa_ext):
     """
-    Add function which uses avx_add_instruction_maps to execute the addition
-    kernel on any type operands.
+    Generic add kernel for adding a scalar to an immediate vector.
     """
     INPUT_TYPE = arg_x.c_type.base
     OUTPUT_TYPE = arg_x.c_type.base
@@ -22,16 +21,19 @@ def add_IVV_IV_generic(arg_x, arg_y, arg_n, isa_ext):
         SIMD_LOAD, SIMD_ADD, SIMD_STORE, _ = avx_vector_instruction_select(INPUT_TYPE, OUTPUT_TYPE)
         reg_x_scalar = avx_scalar_register_map[OUTPUT_TYPE]()
         reg_y_scalar = avx_scalar_register_map[OUTPUT_TYPE]()
+        reg_y_vector = YMMRegister()
         simd_accs = [YMMRegister() for _ in range(UNROLL_FACTOR)]
-        simd_ops = [YMMRegister() for _ in range(UNROLL_FACTOR)]
     elif isa_ext == "sse":
         SIMD_REGISTER_SIZE = XMMRegister.size
         SCALAR_LOAD, SCALAR_ADD, SCALAR_STORE = sse_scalar_instruction_select(INPUT_TYPE, OUTPUT_TYPE)
         SIMD_LOAD, SIMD_ADD, SIMD_STORE = sse_vector_instruction_select(INPUT_TYPE, OUTPUT_TYPE)
         reg_x_scalar = sse_scalar_register_map[OUTPUT_TYPE]()
         reg_y_scalar = sse_scalar_register_map[OUTPUT_TYPE]()
+        if INPUT_TYPE in [ Yep32f, Yep64f ]:
+            reg_y_vector = reg_y_scalar
+        else:
+            reg_y_vector = XMMRegister()
         simd_accs = [XMMRegister() for _ in range(UNROLL_FACTOR)]
-        simd_ops = [XMMRegister() for _ in range(UNROLL_FACTOR)]
 
 
     ret_ok = Label()
@@ -52,11 +54,10 @@ def add_IVV_IV_generic(arg_x, arg_y, arg_n, isa_ext):
     TEST(reg_x_addr, OUTPUT_TYPE_SIZE - 1) # Check that our output arr is aligned
     JNZ(ret_misaligned_pointer)
 
-    reg_y_addr = GeneralPurposeRegister64()
-    LOAD.ARGUMENT(reg_y_addr, arg_y)
-    TEST(reg_y_addr, reg_y_addr) # Make sure arg_y is not null
-    JZ(ret_null_pointer)
-
+    if INPUT_TYPE_SIZE < 4:
+        LOAD.ARGUMENT(reg_y_scalar.as_dword, arg_y)
+    else:
+        LOAD.ARGUMENT(reg_y_scalar, arg_y)
 
     align_loop = Loop() # Loop to align one of the addresses
     scalar_loop = Loop() # Processes remainder elements (if n % 8 != 0)
@@ -67,11 +68,9 @@ def add_IVV_IV_generic(arg_x, arg_y, arg_n, isa_ext):
     JZ(align_loop.end) # If so, skip this loop entirely
     with align_loop:
         SCALAR_LOAD(reg_x_scalar, [reg_x_addr])
-        SCALAR_LOAD(reg_y_scalar, [reg_y_addr])
         SCALAR_ADD(reg_x_scalar, reg_x_scalar, reg_y_scalar)
         SCALAR_STORE([reg_x_addr], reg_x_scalar)
         ADD(reg_x_addr, OUTPUT_TYPE_SIZE)
-        ADD(reg_y_addr, OUTPUT_TYPE_SIZE)
         SUB(reg_length, 1)
         JZ(ret_ok)
         TEST(reg_x_addr, SIMD_REGISTER_SIZE - 1)
@@ -80,23 +79,24 @@ def add_IVV_IV_generic(arg_x, arg_y, arg_n, isa_ext):
     reg_x_addr_out = GeneralPurposeRegister64()
     MOV(reg_x_addr_out, reg_x_addr)
 
+    if isa_ext == "avx":
+        AVX_MOV_GPR_TO_VECTOR(reg_y_vector, reg_y_scalar, INPUT_TYPE, OUTPUT_TYPE)
+    elif isa_ext == "sse":
+        SSE_MOV_GPR_TO_VECTOR(reg_y_vector, reg_y_scalar, INPUT_TYPE, OUTPUT_TYPE)
+
     # Batch loop for processing the rest of the array in a pipelined loop
-    instruction_columns = [InstructionStream(), InstructionStream(), InstructionStream(), InstructionStream()]
-    instruction_offsets = (0, 1, 2, 3)
+    instruction_columns = [InstructionStream(), InstructionStream(), InstructionStream()]
+    instruction_offsets = (0, 1, 2)
     for i in range(UNROLL_FACTOR):
         with instruction_columns[0]:
             SIMD_LOAD(simd_accs[i], [reg_x_addr + i * SIMD_REGISTER_SIZE * INPUT_TYPE_SIZE / OUTPUT_TYPE_SIZE])
         with instruction_columns[1]:
-            SIMD_LOAD(simd_ops[i], [reg_y_addr + i * SIMD_REGISTER_SIZE * INPUT_TYPE_SIZE / OUTPUT_TYPE_SIZE])
+            SIMD_ADD(simd_accs[i], simd_accs[i], reg_y_vector)
         with instruction_columns[2]:
-            SIMD_ADD(simd_accs[i], simd_accs[i], simd_ops[i])
-        with instruction_columns[3]:
             SIMD_STORE([reg_x_addr_out + i * SIMD_REGISTER_SIZE], simd_accs[i])
     with instruction_columns[0]:
         ADD(reg_x_addr, SIMD_REGISTER_SIZE * UNROLL_FACTOR * INPUT_TYPE_SIZE / OUTPUT_TYPE_SIZE)
-    with instruction_columns[1]:
-        ADD(reg_y_addr, SIMD_REGISTER_SIZE * UNROLL_FACTOR * INPUT_TYPE_SIZE / OUTPUT_TYPE_SIZE)
-    with instruction_columns[3]:
+    with instruction_columns[2]:
         ADD(reg_x_addr_out, SIMD_REGISTER_SIZE * UNROLL_FACTOR * INPUT_TYPE_SIZE / OUTPUT_TYPE_SIZE)
 
     software_pipelined_loop(reg_length, UNROLL_FACTOR * SIMD_REGISTER_SIZE / OUTPUT_TYPE_SIZE, instruction_columns, instruction_offsets)
@@ -107,11 +107,9 @@ def add_IVV_IV_generic(arg_x, arg_y, arg_n, isa_ext):
     JZ(scalar_loop.end)
     with scalar_loop: # Process the remaining elements
         SCALAR_LOAD(reg_x_scalar, [reg_x_addr])
-        SCALAR_LOAD(reg_y_scalar, [reg_y_addr])
         SCALAR_ADD(reg_x_scalar, reg_x_scalar, reg_y_scalar)
         SCALAR_STORE([reg_x_addr], reg_x_scalar)
         ADD(reg_x_addr, OUTPUT_TYPE_SIZE)
-        ADD(reg_y_addr, OUTPUT_TYPE_SIZE)
         SUB(reg_length, 1)
         JNZ(scalar_loop.begin)
 
--- library/sources/core/yepCore_Add_Haswell.py
+++ library/sources/core/yepCore_Add_Haswell.py
@@ -3,6 +3,7 @@ from peachpy import *
 from kernels.add.add_VV_V_generic import add_VV_V_generic
 from kernels.add.add_VS_V_generic import add_VS_V_generic
 from kernels.add.add_IVV_IV_generic import add_IVV_IV_generic
+from kernels.add.add_IVS_IV_generic import add_IVS_IV_generic
 from common.YepStatus import *
 
 # =======================================================================
@@ -343,3 +344,68 @@ with Function("yepCore_Add_IV64fV64f_IV64f",
         (arg_x, arg_y, arg_n),
         YepStatus, target=uarch.haswell + isa.avx2) as Add_IV64fV64f_IV64f:
     add_IVV_IV_generic(arg_x, arg_y, arg_n, "avx")
+
+
+# =======================================================================
+# =======================================================================
+# ADD SCALAR TO IMMEDIATE VECTOR
+# =======================================================================
+# =======================================================================
+arg_x = Argument(ptr(Yep8s), name="xPointer")
+arg_y = Argument(Yep8s, name="yPointer")
+arg_n = Argument(YepSize, name="length")
+
+with Function("yepCore_Add_IV8sS8s_IV8s",
+        (arg_x, arg_y, arg_n),
+        YepStatus, target=uarch.haswell + isa.avx2) as Add_IV8sS8s_IV8s:
+    add_IVS_IV_generic(arg_x, arg_y, arg_n, "avx")
+
+
+arg_x = Argument(ptr(Yep16s), name="xPointer")
+arg_y = Argument(Yep16s, name="yPointer")
+arg_n = Argument(YepSize, name="length")
+
+with Function("yepCore_Add_IV16sS16s_IV16s",
+        (arg_x, arg_y, arg_n),
+        YepStatus, target=uarch.haswell + isa.avx2) as Add_IV16sS16s_IV16s:
+    add_IVS_IV_generic(arg_x, arg_y, arg_n, "avx")
+
+
+arg_x = Argument(ptr(Yep32s), name="xPointer")
+arg_y = Argument(Yep32s, name="yPointer")
+arg_n = Argument(YepSize, name="length")
+
+with Function("yepCore_Add_IV32sS32s_IV32s",
+        (arg_x, arg_y, arg_n),
+        YepStatus, target=uarch.haswell + isa.avx2) as Add_IV32sS32s_IV32s:
+    add_IVS_IV_generic(arg_x, arg_y, arg_n, "avx")
+
+
+arg_x = Argument(ptr(Yep64s), name="xPointer")
+arg_y = Argument(Yep64s, name="yPointer")
+arg_n = Argument(YepSize, name="length")
+
+with Function("yepCore_Add_IV64sS64s_IV64s",
+        (arg_x, arg_y, arg_n),
+        YepStatus, target=uarch.haswell + isa.avx2) as Add_IV64sS64s_IV64s:
+    add_IVS_IV_generic(arg_x, arg_y, arg_n, "avx")
+
+
+arg_x = Argument(ptr(Yep32f), name="xPointer")
+arg_y = Argument(Yep32f, name="yPointer")
+arg_n = Argument(YepSize, name="length")
+
+with Function("yepCore_Add_IV32fS32f_IV32f",
+        (arg_x, arg_y, arg_n),
+        YepStatus, target=uarch.haswell + isa.avx2) as Add_IV32fS32f_IV32f:
+    add_IVS_IV_generic(arg_x, arg_y, arg_n, "avx")
+
+
+arg_x = Argument(ptr(Yep64f), name="xPointer")
+arg_y = Argument(Yep64f, name="yPointer")
+arg_n = Argument(YepSize, name="length")
+
+with Function("yepCore_Add_IV64fS64f_IV64f",
+        (arg_x, arg_y, arg_n),
+        YepStatus, target=uarch.haswell + isa.avx2) as Add_IV64fS64f_IV64f:
+    add_IVS_IV_generic(arg_x, arg_y, arg_n, "avx")
