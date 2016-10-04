7346acc1e75b885c21a92970af369b7bc6319617
Robert Guthrie <rguthrie3@gatech.edu>
2016-10-03 23:48:24 -0400

    Added SumSquared for haswell

--- library/sources/core/yepCore_Sum.py
+++ library/sources/core/yepCore_Sum.py
@@ -5,7 +5,7 @@ from peachpy.function import Argument
 from peachpy.c.types import ptr
 import peachpy.c.types as ctypes
 from common.YepStatus import YepStatus
-from kernels.sum_reduction import sum_Haswell
+from kernels.sum_reduction import sum_Haswell, sum_squared_Haswell
 
 arg_x = Argument(ptr(ctypes.const_Yep32f), name="xPointer")
 arg_z = Argument(ptr(ctypes.Yep32f), name="zPointer")
@@ -16,6 +16,11 @@ with Function("yepCore_Sum_V32f_S32f",
         YepStatus, target=uarch.haswell + isa.avx2) as yepCore_Sum_V32f_S32f:
     sum_Haswell(arg_x, arg_z, arg_n)
 
+with Function("yepCore_SumSquares_V32f_S32f",
+        (arg_x, arg_z, arg_n),
+        YepStatus, target=uarch.haswell + isa.avx2) as yepCore_SumSquares_V32f_S32f:
+    sum_squared_Haswell(arg_x, arg_z, arg_n)
+
 
 arg_x = Argument(ptr(ctypes.const_Yep64f), name="xPointer")
 arg_z = Argument(ptr(ctypes.Yep64f), name="zPointer")
@@ -25,3 +30,8 @@ with Function("yepCore_Sum_V64f_S64f",
         (arg_x, arg_z, arg_n),
         YepStatus, target=uarch.haswell + isa.avx2) as yepCore_Sum_V64f_S64f:
     sum_Haswell(arg_x, arg_z, arg_n)
+
+with Function("yepCore_SumSquares_V64f_S64f",
+        (arg_x, arg_z, arg_n),
+        YepStatus, target=uarch.haswell + isa.avx2) as yepCore_SumSquares_V64f_S64f:
+    sum_squared_Haswell(arg_x, arg_z, arg_n)
